08d408c27c29ccfc1d5ea9c2aaa62493c38a7a0f
kimmy214 <upcwangying@126.com>
2022-10-19 11:38:32 +0800

    feat(backend): add mongodb support

--- backend/functions/hello.ts
+++ backend/functions/hello.ts
@@ -1,9 +1,28 @@
 import { Handler } from '@netlify/functions'
+import { MongoClient, MongoServerError } from 'mongodb'
+
+// @ts-ignore
+const client = new MongoClient(process.env.MONGODB_URI)
+const clientPromise = client.connect()
 
 const handler: Handler = async (event, context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({ message: 'Hello World' })
+  try {
+    const database = (await clientPromise).db(process.env.MONGODB_DATABASE)
+    // @ts-ignore
+    const collection = database.collection(process.env.MONGODB_COLLECTION)
+    const results = collection.find().sort('score', 'descending').limit(10).toArray()
+
+    return {
+      statusCode: 200,
+      body: JSON.stringify(results)
+    }
+  } catch (error) {
+    if (error instanceof MongoServerError) {
+      return { statusCode: 500, body: error }
+    }
+    return { statusCode: 500, body: (error as any).message }
+  } finally {
+    await client.close();
   }
 }
 
